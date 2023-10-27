1ab238190fb51d56d06593ce105c1b9f17194fe0
Jocyline Magero <jmagero54@gmail.com>
2023-10-27 10:34:01 -0500

    Exclude Security

---
+++ src/main/java/io/jmagero/securecapita/SecurecapitaApplication.java
@@ -0,0 +1,15 @@
+package io.jmagero.securecapita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class SecurecapitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurecapitaApplication.class, args);
+	}
+
+
+}
